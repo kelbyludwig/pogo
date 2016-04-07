37817bf9bd59432281fd75c7ffc2021710a5385d
Kelby Ludwig <kelbyludwig@gmail.com>
2016-04-07 12:56:20 -0500

    Added PKCS7 padding validation

--- pogo.go
+++ pogo.go
@@ -13,6 +13,60 @@ type Oracle func([]byte) error
 type Padding func([]byte, int) []byte
 type Unpadding func([]byte, int) ([]byte, error)
 
+func PaddingOracleBlockReveal(blocks [][]byte, targetBlockIndex int, oracle Oracle) (plaintext []byte, err error) {
+
+	targetBlock := blocks[targetBlockIndex]
+	modBlock := blocks[targetBlockIndex-1]
+
+	if len(blocks) >= targetBlockIndex {
+		err = fmt.Errorf("target block index was too large")
+		return
+	}
+
+	ltb := len(targetBlock)
+	lmb := len(modBlock)
+
+	//In order to reduce the chance false positives, lets scramble the modBlock such that none of the original bytes are the same
+	for i := 0; i < lmb; i++ {
+		modBlock[i] = modBlock[i] + 1
+	}
+
+	//poi ("padding oracle index") keeps track of the index of the target byte
+	poi := ltb
+	expectedPadding := byte(1)
+
+	for poi >= 0 {
+		intermediateState := make([]byte, ltb)
+		for mb := 0; mb < 256; mb++ {
+			modByte := byte(mb)
+			modBlock[poi] = modByte
+			blocks[targetBlockIndex-1] = modBlock
+			ciphertext := MergeBlocks(blocks)
+			err := oracle(ciphertext)
+			if err != nil {
+				isb := modByte ^ expectedPadding
+				intermediateState[poi] = isb
+				expectedPadding += 1
+				modBlock[poi] = expectedPadding ^ isb
+				poi--
+				break
+			}
+		}
+		err = fmt.Errorf("unable to find valid padding for the target block")
+		return
+
+	}
+	return
+}
+
+func MergeBlocks(input [][]byte) []byte {
+	output := make([]byte, 0)
+	for _, b := range input {
+		output = append(output, b...)
+	}
+	return output
+}
+
 func SplitBlocks(input []byte, blockSize int) (blocks [][]byte, err error) {
 
 	if len(input)%blockSize != 0 {
@@ -44,4 +98,29 @@ func PKCS7Unpadding(src []byte, blockSize int) ([]byte, error) {
 		return nil, fmt.Errorf("padding size error")
 	}
 	return src[:srcLen-paddingLen], nil
+}
+
+func PKCS7Validate(input []byte, blockSize int) error {
+	err := fmt.Errorf("Invalid padding")
+	l := len(input)
+	lb := input[l-1]
+
+	if lb == byte(0) {
+		return err
+	}
+
+	if l%blockSize != 0 {
+		return err
+	}
+
+	if int(lb) > blockSize {
+		return err
+	}
+
+	for i := l - 1; i > l-1-int(lb); i-- {
+		if input[i] != lb {
+			return err
+		}
+	}
+	return nil
 }
--- pogo_test.go
+++ pogo_test.go
@@ -9,6 +9,32 @@ import (
 	"testing"
 )
 
+func TestPKCS7PaddingValidation(t *testing.T) {
+	blockSize := 16
+	for i := 0; i < blockSize; i++ {
+		nulls := make([]byte, i)
+		valid := PKCS7Padding(nulls, blockSize)
+		if err := PKCS7Validate(valid, blockSize); err != nil {
+			t.Errorf("A valid padded block failed validation")
+			return
+		}
+
+	}
+	blockSize = 8
+	i1 := []byte("ABCD\x01\x02\x03\x04")
+	i2 := []byte("ABCDE\x02\x03\x03")
+	i3 := []byte("ABCDE\x04\x04\x04")
+	i4 := []byte("ABCDEFG\x00")
+	invalids := [][]byte{i1, i2, i3, i4}
+	for i, x := range invalids {
+		err := PKCS7Validate(x, blockSize)
+		if err == nil {
+			t.Errorf("A invalid padded block passed validation (i%v)", i)
+			return
+		}
+	}
+}
+
 func TestSplitBlocksError(t *testing.T) {
 	input := []byte("ABC")
 	bs := 2
@@ -37,56 +63,51 @@ func TestSplitBlocks(t *testing.T) {
 
 	if string(output[0]) != "AB" || string(output[1]) != "CD" {
 		t.Errorf("blocks were not split properly")
+		return
 	}
 
+	rev := MergeBlocks(output)
+	if string(rev) != "ABCD" {
+		t.Errorf("failed to properly merge split blocks")
+		return
+	}
 }
 
 func TestCBCPaddingOracle(t *testing.T) {
-	key := []byte("example key 1234")
-	plaintextNoPadding := []byte("ay lmao")
-	plaintext := PKCS7Padding(plaintextNoPadding, aes.BlockSize)
 
-	if len(plaintext)%aes.BlockSize != 0 {
-		t.Errorf("plaintext is not a multiple of the block size")
-		return
-	}
+	//oracle := func(input []byte) error {
+	//	key := []byte("example key 1234")
+	//	plaintextNoPadding := []byte("This is my plaintext. There are many like it, but this is mine.")
+	//	plaintext := PKCS7Padding(plaintextNoPadding, aes.BlockSize)
+	//	block, _ := aes.NewCipher(key)
+	//	iv := make([]byte, aes.BlockSize)
+	//	ciphertext := make([]byte, len(plaintext))
+	//	enc := cipher.NewCBCEncrypter(block)
+	//	enc.CryptBlocks(iv, plaintext)
 
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		t.Errorf("%v", err)
-		return
-	}
+	//}
 
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
+	//verPlaintext := make([]byte, len(ciphertext)-len(iv))
+	//dec := cipher.NewCBCDecrypter(block, iv)
+	//dec.CryptBlocks(verPlaintext, ciphertext[aes.BlockSize:])
 
-	enc := cipher.NewCBCEncrypter(block, iv)
-	enc.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+	//if string(verPlaintext) != string(plaintext) {
+	//	t.Errorf("decrypted padded plaintext did not match expected plaintext")
+	//	log.Printf("Expected %v | Recieved %v\n", plaintext, verPlaintext)
+	//	return
+	//}
 
-	verPlaintext := make([]byte, len(ciphertext)-len(iv))
-	dec := cipher.NewCBCDecrypter(block, iv)
-	dec.CryptBlocks(verPlaintext, ciphertext[aes.BlockSize:])
+	//verPlaintextNoPadding, err := PKCS7Unpadding(verPlaintext, aes.BlockSize)
 
-	if string(verPlaintext) != string(plaintext) {
-		t.Errorf("decrypted padded plaintext did not match expected plaintext")
-		log.Printf("Expected %v | Recieved %v\n", plaintext, verPlaintext)
-		return
-	}
+	//if err != nil {
+	//	t.Errorf("%v", err)
+	//	return
+	//}
 
-	verPlaintextNoPadding, err := PKCS7Unpadding(verPlaintext, aes.BlockSize)
-
-	if err != nil {
-		t.Errorf("%v", err)
-		return
-	}
-
-	if string(verPlaintextNoPadding) != string(plaintextNoPadding) {
-		t.Errorf("decrypted unpadded plaintexted did not match expected plaintext")
-		return
-	}
+	//if string(verPlaintextNoPadding) != string(plaintextNoPadding) {
+	//	t.Errorf("decrypted unpadded plaintexted did not match expected plaintext")
+	//	return
+	//}
 
 }
 
